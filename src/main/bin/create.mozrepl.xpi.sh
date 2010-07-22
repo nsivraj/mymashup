7c39e0ae2a3aa6a131bc01c8189d86c0c3698c98
nsivraj <norman.jarvis@gmail.com>
2010-07-21 21:26:40 -0600

    Adding in the first major commit items for mymashup

--- src/main/bin/create.mozrepl.xpi.sh
+++ src/main/bin/create.mozrepl.xpi.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
+GITREPO_HOME=/home/nsivraj/forge/personal/code/repo_git
+MOZREPL_HOME=$GITREPO_HOME/mozrepl
+cd $GITREPO_HOME
 rm mozrepl.xpi
-cd mozrepl
-zip a -tzip mozrepl.zip *
-mv mozrepl.zip ../mozrepl.xpi
-cd ..
+cd $MOZREPL_HOME
+zip -r -q mozrepl .
+mv mozrepl.zip $GITREPO_HOME/mozrepl.xpi
+cd $GITREPO_HOME
